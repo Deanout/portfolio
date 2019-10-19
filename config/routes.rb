26d4089103d737f5e02538da09e3b8d3b01d9c47
Deanout <dean@deanout.com>
2019-10-19 17:07:35 -0500

    Init commit

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  resources :posts
+  get 'blog', to: 'posts#index', as: 'blog'
+  root 'pages#home'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
